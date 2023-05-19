080040cc9c283b1fb588cb6374dffc35a172fc99
yushsato <yushsato@student.42tokyo.jp>
2023-05-19 15:57:00 +0900

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,8 +6,27 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 15:22:43 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/19 15:23:01 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/19 15:33:07 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_atoi(const char *str)
+{
+	int	ret;
+	int	sign;
 
+	ret = 0;
+	sign = 1;
+	while (*str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		sign = -1;
+		str++;
+	}
+	if (*str == '0')
+		return (0);
+	while ('0' <= *str && *str <= '9')
+		ret = ret * 10 + *str - '0';
+	return (ret * sign)
+}
