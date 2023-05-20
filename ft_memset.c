844544cff87c482385d40959e2b709869518fe79
yushsato <yushsato@student.42tokyo.jp>
2023-05-20 20:26:44 +0900

    update

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/18 14:05:14 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/20 16:38:42 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/20 20:26:42 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*s;
 
