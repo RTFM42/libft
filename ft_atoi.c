0333939eb995ad4aec858d60cf701e77883e9c14
yushin sato <yussato@Strong-Swan.local>
2023-05-28 21:29:30 +0900

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,37 +6,56 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 15:22:43 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/28 17:31:47 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/28 21:13:21 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
+
+int	overflowl(int sign, long l, char next)
+{
+	if (sign == 1)
+		if (LONG_MAX / 10 < l
+			|| (LONG_MAX / 10 == l && LONG_MAX % 10 <= next - '0'))
+			return ((int)LONG_MAX);
+	if (sign == -1)
+		if (LONG_MIN / -10 < l
+			|| (LONG_MIN / -10 == l && LONG_MIN % -10 * -1 <= next - '0'))
+			return ((int)LONG_MIN);
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	long long	ret;
-	int			sign;
+	long	ret;
+	int		sign;
 
 	ret = 0;
 	sign = 1;
-	while (*str == ' ' || *str == '\t'
-		|| *str == '\n' || *str == '\r' || *str == '\r')
+	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
 		if (*str++ == '-')
 			sign = -1;
 	while (*str)
 	{
-		if ('0' <= *str && *str++ <= '9')
+		if ('0' <= *str && *str <= '9')
 		{
-			ret = ret * 10 + *(str - 1) - '0';
-			if (ret >= LONG_MAX)
-				return ((int)LONG_MAX);
-			if (ret <= LONG_MIN)
-				return ((int)LONG_MIN);
+			if (overflowl(sign, ret, *str))
+				return (overflowl(sign, ret, *str));
+			ret = ret * 10 + *str - '0';
+			str++;
 		}
 		else
 			break ;
 	}
-	return ((int)(long)(ret * sign));
+	return ((int)(ret * sign));
 }
