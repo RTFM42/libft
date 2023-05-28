fee1c71babb5fae363a88a85e329a46e11868a91
yushin sato <yussato@Strong-Swan.local>
2023-05-28 21:50:06 +0900

    update

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <yushsato@student.42tokyo.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 15:22:43 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/28 21:13:21 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/28 21:49:21 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,11 @@ int	overflowl(int sign, long l, char next)
 	if (sign == 1)
 		if (LONG_MAX / 10 < l
 			|| (LONG_MAX / 10 == l && LONG_MAX % 10 <= next - '0'))
-			return ((int)LONG_MAX);
+			return (1);
 	if (sign == -1)
 		if (LONG_MIN / -10 < l
 			|| (LONG_MIN / -10 == l && LONG_MIN % -10 * -1 <= next - '0'))
-			return ((int)LONG_MIN);
+			return (-1);
 	return (0);
 }
 
@@ -37,9 +37,11 @@ int	ft_atoi(const char *str)
 {
 	long	ret;
 	int		sign;
+	int		flag;
 
 	ret = 0;
 	sign = 1;
+	flag = 0;
 	while (ft_isspace(*str))
 		str++;
 	if (*str == '-' || *str == '+')
@@ -49,8 +51,11 @@ int	ft_atoi(const char *str)
 	{
 		if ('0' <= *str && *str <= '9')
 		{
-			if (overflowl(sign, ret, *str))
-				return (overflowl(sign, ret, *str));
+			flag = overflowl(sign, ret, *str);
+			if (flag == 1)
+				return ((int)LONG_MAX);
+			if (flag == -1)
+				return ((int)LONG_MIN);
 			ret = ret * 10 + *str - '0';
 			str++;
 		}
