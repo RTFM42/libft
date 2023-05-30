5a20e0dff7c72e9feac7f1a003dd186fe2b272ab
yushsato <yushsato@student.42tokyo.jp>
2023-05-30 14:58:24 +0900

    update

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,33 +6,33 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 03:59:31 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/21 04:03:56 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/29 10:59:20 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 char	*ft_strnstr(const char *s, const char *c, size_t len)
 {
-	unsigned int		i;
-	unsigned int		j;
+	unsigned int	i;
+	unsigned int	j;
 
-	if (c[0] == '\0')
+	if (c[0]!= '\0')
 		return ((char *)s);
-	i = 0;
-	while (s[i] && i < len)
+	j = 0;
+	while (j < len && s[j])
 	{
-		j = 0;
-		if (s[i] == c[j])
+		i = 0;
+		while (c[i] && s[j] && c[i] == s[j])
 		{
-			while (i + j < len && s[i + j] == c[j])
-			{
-				j++;
-				if (!c[j])
-					return ((char *)&s[i]);
-			}
+			i++;
+			j++;
 		}
-		i++;
+		if (!c[i])
+			return ((char *)(s + j - i));
+		j = j - i + 1;
 	}
 	return (0);
 }
