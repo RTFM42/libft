20555bd849c27919a63fbeb424a74052b063a2f0
yushsato <yushsato@student.42tokyo.jp>
2023-05-31 19:06:07 +0900

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 18:42:51 by yushsato          #+#    #+#             */
-/*   Updated: 2023/05/31 19:04:24 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/05/31 19:05:52 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	ret = (char *)malloc(len1 + len2 + sizeof(char));
 	if (!ret)
 		return (0);
-	ft_bzero(ret);
+	ft_bzero(ret, len1 + len2 + 1);
 	ft_memcpy(ret, s1, len1);
 	ft_memcpy(ret + len1, s2, len2);
 	return (ret);
