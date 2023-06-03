d7df053284405fa4433c4122a7783ae2e9d008c1
yushin sato <yussato@Strong-Swan.local>
2023-06-04 01:32:07 +0900

    update

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 17:26:14 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/03 23:30:44 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/04 01:32:02 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 char	**ft_split(const char *s, char c)
 {
-	void			ret;
+	void			*ret;
 	unsigned int	nptr;
 	unsigned int	size;
+
+
 }
