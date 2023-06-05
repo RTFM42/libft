a60085e4baf06b0960f5e6e94dd89b5204952cb8
yushsato <yushsato@student.42tokyo.jp>
2023-06-05 21:43:01 +0900

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,58 +6,38 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 16:53:32 by yushsato          #+#    #+#             */
-/*   Updated: 2023/06/05 18:18:53 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/06/05 21:42:59 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	intlen(int n)
-{
-	int	i;
-
-	i = 0;
-	if (n < 0)
-	{
-		n *= -1;
-		i++;
-	}
-	if (n == 0)
-		return (1);
-	while (n)
-	{
-		n = n / 10;
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_itoa(int n)
 {
-	char	*res;
-	char	*ret;
 	int		len;
+	int		nc;
+	int		flag;
+	char	*ret;
 
-	len = intlen(n);
-	res = ft_calloc(sizeof(char), len + 1);
-	if (!res)
-		return (NULL);
-	ret = res;
-	if (n == 0)
-	{
-		*ret = '0';
-		return (ret);
-	}
+	len = 0;
+	flag = 1;
 	if (n < 0)
 	{
 		n *= -1;
-		*res++ = '-';
-		len--;
+		len++;
 	}
-	while (len)
+	nc = n;
+	while (flag || n)
 	{
-		ret[--len] = '0' + n / 10;
-		n = n / 10;
+		flag = 0;
+		n /= 10;
+		len++;
 	}
-	return (ret);
+	ret = malloc(len + 1);
+	if (!ret)
+		return (NULL);
+	n = nc;
+	if (n < 0)
+		*ret++ = '-';
+	while (
 }
